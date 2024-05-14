0eee7ae414274db7770a30f0755569b94e47743d
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-14 14:51:29 -0400

    no ingredients

--- src/components/BurgerStack/BurgerStack.jsx
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -4,13 +4,16 @@ import Ingredient from "../Ingredient/Ingredient.jsx"
 const BurgerStack = ({ stack, handleButton }) => {
     return <>
         <ul>
-            {stack.map( (ingredient, index) => (
-                <Ingredient key={index}
-                    {...ingredient} buttonText='X'
-                    handleButton={handleButton}
-                    index={index}
-                />
-            ))}
+            {stack.length ? 
+                stack.map( (ingredient, index) => (
+                    <Ingredient key={index}
+                        {...ingredient} buttonText='X'
+                        handleButton={handleButton}
+                        index={index}
+                    />
+                ))
+                : <li>No ingredients</li>
+            }
         </ul>
     </>
 }
