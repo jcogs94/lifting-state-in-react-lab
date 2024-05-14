b5bdaedbc9a30024d484708f6645a8dd92727199
jcogs94 <38306102+jcogs94@users.noreply.github.com>
2024-05-14 12:41:08 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import './App.css'
+
+const App = () => {
+  return (
+    <main>
+      <h1>Burger Stacker</h1>
+      <section>
+      {/* List & Stack components */}
+      </section>
+    </main>
+  )
+}
+
+export default App
